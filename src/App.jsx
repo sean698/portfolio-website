904e4e7e8256092d31bb72eb80fc46bd36bcc8fc
Shiyuan Miao <shiyuanm000@gmail.com>
2024-10-31 21:45:52 -0700

    modify navbar

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,8 @@ const App = () => {
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
-
-      <div className="container mx-auto px-8">
-        <Navbar />
+      <Navbar />
+      <div className="container mx-auto px-8 mt-20">
         <Hero />
         <About />
         <Skills />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,10 @@
-import { HERO_CONTENT } from "../constants";
+import { useState, useEffect } from "react";
+import { ROLES } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
 import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+import { CheckIcon, ChevronRightIcon } from "lucide-react";
+import { AnimatedSubscribeButton } from "@/components/ui/animated-subscribe-button";
 
 const xTransitionContainer = ({ hiddenX = -100, duration = 0.5, delay }) => ({
   hidden: { x: hiddenX, opacity: 0 },
@@ -12,35 +16,95 @@ const xTransitionContainer = ({ hiddenX = -100, duration = 0.5, delay }) => ({
 });
 
 const Hero = () => {
+  const [startTyping, setStartTyping] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStartTyping(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <motion.h1
+            {/* <motion.h1
               variants={xTransitionContainer({ delay: 0 })}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              className="pb-12 text-6xl font-bold tracking-tight lg:mt-16 lg:text-6xl"
             >
-              Shiyuan Miao
-            </motion.h1>
-            <motion.span
+              Hi, I am Shiyuan Miao
+            </motion.h1> */}
+            <motion.div
+              variants={xTransitionContainer({ delay: 0 })}
+              initial="hidden"
+              animate="visible"
+              className="text-4xl mt-16 mb-8"
+            >
+              Hi, I am
+              <h1 className="pb-12 text-6xl font-bold tracking-tight lg:mt-4 lg:text-6xl">
+                Shiyuan Miao
+              </h1>
+            </motion.div>
+            <motion.div
               variants={xTransitionContainer({ delay: 0.5 })}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+              className=""
             >
-              Full Stack Developer
-            </motion.span>
-            <motion.p
+              <div className="flex text-4xl tracking-tight">
+                I am a
+                <p className="ml-3 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                  {startTyping && (
+                    <Typewriter
+                      options={{
+                        strings: ROLES,
+                        autoStart: true,
+                        loop: true,
+                        delay: 80,
+                        deleteSpeed: 40,
+                      }}
+                    />
+                  )}
+                </p>
+              </div>
+            </motion.div>
+            {/* <motion.p
               variants={xTransitionContainer({ delay: 1 })}
               initial="hidden"
               animate="visible"
               className="my-2 max-w-xl py-6 font-light tracking-tighter"
             >
               {HERO_CONTENT}
-            </motion.p>
+            </motion.p> */}
+
+            <motion.div
+              className="mt-11"
+              variants={xTransitionContainer({ delay: 1 })}
+              initial="hidden"
+              animate="visible"
+            >
+              <AnimatedSubscribeButton
+                buttonColor="#000000"
+                buttonTextColor="#ffffff"
+                subscribeStatus={false}
+                initialText={
+                  <span className="group inline-flex items-center">
+                    Get My Resume{" "}
+                    <ChevronRightIcon className="ml-1 size-4 transition-transform duration-300 group-hover:translate-x-1" />
+                  </span>
+                }
+                changeText={
+                  <span className="group inline-flex items-center">
+                    <CheckIcon className="mr-2 size-4" />
+                    Thank you!{" "}
+                  </span>
+                }
+              />
+            </motion.div>
           </div>
         </div>
 

faf5db75309c31fee347eee33a2397d56d3eb1f2
Shiyuan Miao <shiyuanm000@gmail.com>
2024-11-09 11:00:27 -0800

    add gradient bg color to side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,7 @@ import { useState } from "react";
 import { Sidebar, SidebarBody, SidebarLink } from "./ui/sidebar";
 import profilePic from "../assets/profilePic.png";
 import {
-  IconArrowLeft,
   IconTool,
-  IconSettings,
   IconSchool,
   IconUserBolt,
   IconBrandGithub,
@@ -93,9 +91,8 @@ export default function SidebarDemo() {
       )}
     >
       <Sidebar open={open} setOpen={setOpen}>
-        <SidebarBody className="justify-between gap-10">
+        <SidebarBody className="bg-gradient-to-b from-fuchsia-50 via-violet-50 to-indigo-50 justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-x-hidden justify-between">
-            {/* {open ? <Logo /> : <LogoIcon />} */}
             <div className="flex flex-col gap-2">
               {links.map((link, idx) => (
                 <SidebarLink key={idx} link={link} />
@@ -155,31 +152,5 @@ export const LogoIcon = () => {
     >
       <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
     </a>
-  );
-};
-
-// Dummy dashboard component with content
-const Dashboard = () => {
-  return (
-    <div className="flex flex-1">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
-          {[...new Array(4)].map((i) => (
-            <div
-              key={"first-array" + i}
-              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
-        <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((i) => (
-            <div
-              key={"second-array" + i}
-              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
-      </div>
-    </div>
   );
 };
